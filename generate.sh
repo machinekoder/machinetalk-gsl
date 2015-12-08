99b61b43f904eb5fb148b8456ea705a7b09e31d5
Alexander Rössler <mail.aroessler@gmail.com>
2015-12-07 23:14:42 -0600

    updated machinetalk scripts

--- generate.sh
+++ generate.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-gsl -script:test.gsl rpc_client.xml
+#gsl -script:test.gsl rpc_client.xml
 gsl -script:pygen.gsl rpc_client.xml
 gsl -script:dotgen.gsl rpc_client.xml
-dot -Tpng rpcclient.dot > rpcclient.png
+dot -Tpng generated/rpcclient.dot > generated/rpcclient.png
+gsl -script:pygen.gsl rpc_service.xml
+gsl -script:dotgen.gsl rpc_service.xml
+dot -Tpng generated/rpcservice.dot > generated/rpcservice.png
 
