9ea2e5705bf175a472cb103f6bcf74daff8080ba
Alexander Roessler <aroessler@tormach.com>
2015-12-08 16:32:40 -0600

    more work on the machinetalk code generator

--- generate.sh
+++ generate.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 mkdir -p generated
+rm generated/*
 #gsl -script:test.gsl rpc_client.xml
 gsl -script:pygen.gsl rpc_client.xml
+gsl -script:qtgen.gsl rpc_client.xml
 gsl -script:dotgen.gsl rpc_client.xml
-dot -Tpng generated/rpcclient.dot > generated/rpcclient.png
+dot -Tpng generated/machinetalk_rpc_client.dot > generated/machinetalk_rpc_client.png
 gsl -script:pygen.gsl rpc_service.xml
 gsl -script:dotgen.gsl rpc_service.xml
-dot -Tpng generated/rpcservice.dot > generated/rpcservice.png
+dot -Tpng generated/machinetalk_rpc_service.dot > generated/machinetalk_rpc_service.png
 
