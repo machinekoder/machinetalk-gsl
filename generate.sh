469b34bdf32ef38dcf1845f88de01da9ea90c1e8
Alexander Roessler <aroessler@tormach.com>
2015-12-09 17:59:40 -0600

    machinetalk: more work on the generator scripts

--- generate.sh
+++ generate.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+process () {
+name=$1
+gsl -script:pygen.gsl $name.xml
+gsl -script:qtgen.gsl $name.xml
+gsl -script:dotgen.gsl $name.xml
+dot -Tpng generated/machinetalk_$name.dot > generated/machinetalk_$name.png
+}
 mkdir -p generated
 rm generated/*
 #gsl -script:test.gsl rpc_client.xml
-gsl -script:pygen.gsl rpc_client.xml
-gsl -script:qtgen.gsl rpc_client.xml
-gsl -script:dotgen.gsl rpc_client.xml
-dot -Tpng generated/machinetalk_rpc_client.dot > generated/machinetalk_rpc_client.png
-gsl -script:pygen.gsl rpc_service.xml
-gsl -script:dotgen.gsl rpc_service.xml
-dot -Tpng generated/machinetalk_rpc_service.dot > generated/machinetalk_rpc_service.png
-
+process rpc_client
+process rpc_service
+process subscribe
