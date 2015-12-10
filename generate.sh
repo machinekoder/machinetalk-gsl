a74ffec9f0ec5055cd869629fa3edb452a2d4bb4
Alexander Roessler <aroessler@tormach.com>
2015-12-10 15:27:09 -0600

    added a high level generator

--- generate.sh
+++ generate.sh
@@ -6,6 +6,13 @@ gsl -script:qtgen.gsl $name.xml
 gsl -script:dotgen.gsl $name.xml
 dot -Tpng generated/machinetalk_$name.dot > generated/machinetalk_$name.png
 }
+process_hi () {
+name=$1
+#gsl -script:pygen.gsl $name.xml
+gsl -script:qtgen2.gsl $name.xml
+gsl -script:dotgen.gsl $name.xml
+dot -Tpng generated/machinetalk_$name.dot > generated/machinetalk_$name.png
+}
 mkdir -p generated
 rm generated/*
 #gsl -script:test.gsl rpc_client.xml
@@ -13,3 +20,4 @@ process rpc_client
 process rpc_service
 process subscribe
 process publish
+process_hi sync_client
