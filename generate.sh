42f8ed0cd61e5c14c8d5d90cdb96afb059381576
Alexander Roessler <aroessler@tormach.com>
2015-12-15 17:50:41 -0600

    added docgen

--- generate.sh
+++ generate.sh
@@ -4,15 +4,10 @@ name=$1
 gsl -script:pygen.gsl $name.xml
 gsl -script:qtgen.gsl $name.xml
 gsl -script:dotgen.gsl $name.xml
+gsl -script:docgen.gsl $name.xml
 #dot -Tpng generated/machinetalk_$name.dot > generated/machinetalk_$name.png
 }
-process_hi () {
-name=$1
-#gsl -script:pygen.gsl $name.xml
-gsl -script:qtgen2.gsl $name.xml
-gsl -script:dotgen.gsl $name.xml
-#dot -Tpng generated/machinetalk_$name.dot > generated/machinetalk_$name.png
-}
+
 mkdir -p generated
 rm generated/*
 #gsl -script:test.gsl rpc_client.xml
