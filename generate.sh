7bd812dea7a4d99da5b54bf84fbcd2ba2bf21fba
Alexander Roessler <aroessler@tormach.com>
2015-12-17 14:59:09 -0600

    added new protocols and descriptions

--- generate.sh
+++ generate.sh
@@ -4,13 +4,10 @@ name=$1
 gsl -script:pygen.gsl $name.xml
 gsl -script:qtgen.gsl $name.xml
 gsl -script:dotgen.gsl $name.xml
-#gsl -script:docgen.gsl $name.xml
-#dot -Tpng generated/machinetalk_$name.dot > generated/machinetalk_$name.png
 }
 
 mkdir -p generated
 rm generated/*
-#gsl -script:test.gsl rpc_client.xml
 process rpc_client
 process rpc_service
 process subscribe
@@ -18,3 +15,5 @@ process publish
 process sync_client
 process halrcomp
 gsl -script:docgen.gsl rpc_protocol.xml
+gsl -script:docgen.gsl pubsub_protocol.xml
+gsl -script:docgen.gsl halremote_protocol.xml
