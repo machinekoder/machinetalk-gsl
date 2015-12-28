a6523dafaea6c1e3fc33187fd8ec07fecfc2c52f
Alexander Roessler <aroessler@tormach.com>
2015-12-28 15:00:31 -0600

    restructured model folder and added more
    documentation to the halremote protocol

--- generate.sh
+++ generate.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
 process () {
-name=$1
-gsl -script:scripts/pygen.gsl models/$name.xml
-gsl -script:scripts/qtgen.gsl models/$name.xml
-gsl -script:scripts/jsgen.gsl models/$name.xml
-gsl -script:scripts/dotgen.gsl models/$name.xml
+dir=$1
+name=$2
+mkdir -p generated/doc/$dir
+mkdir -p generated/python/$dir
+mkdir -p generated/nodejs/$dir
+mkdir -p generated/qt/$dir
+gsl -script:scripts/pygen.gsl models/$dir/$name.xml
+gsl -script:scripts/qtgen.gsl models/$dir/$name.xml
+gsl -script:scripts/jsgen.gsl models/$dir/$name.xml
+gsl -script:scripts/dotgen.gsl models/$dir/$name.xml
 }
 
-mkdir -p generated/machinetalk
-mkdir -p generated/halremote
-mkdir -p generated/param
-rm generated/machinetalk/*
-rm generated/halremote/*
-rm generated/param/*
-process rpc_client
-process rpc_service
-process subscribe
-process publish
-process sync_client
-process halrcomp
-process halrcomp_subscribe
-process param_client
-process param_server
-gsl -script:scripts/docgen.gsl models/rpc_protocol.xml
-gsl -script:scripts/docgen.gsl models/pubsub_protocol.xml
-gsl -script:scripts/docgen.gsl models/halremote_protocol.xml
-gsl -script:scripts/docgen.gsl models/param_protocol.xml
+process machinetalk rpc_client
+process machinetalk rpc_service
+process machinetalk subscribe
+process machinetalk publish
+process machinetalk sync_client
+process halremote halrcomp
+process halremote halrcomp_subscribe
+process param param_client
+process param param_server
+gsl -script:scripts/docgen.gsl models/machinetalk/rpc_protocol.xml
+gsl -script:scripts/docgen.gsl models/machinetalk/pubsub_protocol.xml
+gsl -script:scripts/docgen.gsl models/halremote/halremote_protocol.xml
+gsl -script:scripts/docgen.gsl models/param/param_protocol.xml
