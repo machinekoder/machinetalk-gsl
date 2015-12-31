b94a224807e8bbf758eb75accedd7168387d4871
Alexander Rössler <mail.aroessler@gmail.com>
2015-12-31 07:09:12 -0600

    implemented error protocol

--- generate.sh
+++ generate.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 process () {
-dir=$1
-name=$2
-mkdir -p generated/doc/$dir
-mkdir -p generated/python/$dir
-mkdir -p generated/nodejs/$dir
-mkdir -p generated/qt/$dir
-gsl -script:scripts/pygen.gsl models/$dir/$name.xml
-gsl -script:scripts/qtgen.gsl models/$dir/$name.xml
-gsl -script:scripts/jsgen.gsl models/$dir/$name.xml
-gsl -script:scripts/dotgen.gsl models/$dir/$name.xml
+    dir=$1
+    name=$2
+    mkdir -p generated/doc/$dir
+    mkdir -p generated/python/$dir
+    mkdir -p generated/nodejs/$dir
+    mkdir -p generated/qt/$dir
+    gsl -script:scripts/pygen.gsl models/$dir/$name.xml
+    gsl -script:scripts/qtgen.gsl models/$dir/$name.xml
+    gsl -script:scripts/jsgen.gsl models/$dir/$name.xml
+    gsl -script:scripts/dotgen.gsl models/$dir/$name.xml
+}
+
+protocol () {
+    dir=$1
+    name=$2
+    gsl -script:scripts/docgen.gsl models/$dir/$name.xml
 }
 
 process machinetalk rpc_client
@@ -24,9 +30,13 @@ process param param_server
 process application launcher
 process application launcher_subscribe
 process application config
-gsl -script:scripts/docgen.gsl models/machinetalk/rpc_protocol.xml
-gsl -script:scripts/docgen.gsl models/machinetalk/pubsub_protocol.xml
-gsl -script:scripts/docgen.gsl models/halremote/halremote_protocol.xml
-gsl -script:scripts/docgen.gsl models/param/param_protocol.xml
-gsl -script:scripts/docgen.gsl models/application/launcher_protocol.xml
-gsl -script:scripts/docgen.gsl models/application/config_protocol.xml
+process application error
+process application error_subscribe
+
+protocol machinetalk rpc_protocol
+protocol machinetalk pubsub_protocol
+protocol halremote halremote_protocol
+protocol param param_protocol
+protocol application launcher_protocol
+protocol application config_protocol
+
